9a2f9c6fbec60ba76d5ad247df86d280e0e257e4
ghaenen <g.f.m.haenen@philips.com>
2021-04-30 14:36:33 +0200

    Update notify-result-teams.cmd

--- .github/bin/notify-result-teams.cmd
+++ .github/bin/notify-result-teams.cmd
@@ -3,4 +3,4 @@
 set COLOR="ff0000"
 IF "%VALIDATION_STATUS%" == "success" set COLOR="00ff00"
 
-curl -H 'Content-Type: application/json' -v %TEAMS_WEBHOOK% --data-binary "{  \"@type\": \"MessageCard\",  \"@context\": \"http://schema.org/extensions\",   \"themeColor\": \"%COLOR%\",   \"summary\":\"Validation run results: ...\",   \"sections\": [    {      \"activityTitle\": \"Validation results %VALIDATION_PART% - Windows\",      \"activitySubtitle\": \"results: %VALIDATION_STATUS%\",      \"markdown\": true    }  ],}"
+curl -H 'Content-Type: application/json' -v %TEAMS_WEBHOOK% --data-binary "{  \"@type\": \"MessageCard\",  \"@context\": \"http://schema.org/extensions\",   \"themeColor\": \"%COLOR%\",   \"summary\":\"Validation run results: ...\",   \"sections\": [    {      \"activityTitle\": \"Validation results %VALIDATION_PART% - %VALIDATION_OS%\",      \"activitySubtitle\": \"results: %VALIDATION_STATUS%\",      \"markdown\": true    }  ],}"
